f884a592acba2e9b6a7ab9e7be7c5f35f363d889
Hiromu OCHIAI <otiai10@gmail.com>
2024-09-12 13:05:43 +0900

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,19 @@
+
+// console.log(chrome.runtime.getManifest());
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+    const platform = (new URL(sender.tab?.url!)).hostname;
+    await chrome.alarms.create(`${platform}/${sender.tab?.id}/join`, { when: message.scheduled });
+    sendResponse({ scheduled: message.scheduled });
+    return true;
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    const [platform, tabId, action] = alarm.name.split('/');
+    chrome.scripting.executeScript({
+        target: { tabId: parseInt(tabId) },
+        func: (p, t, a) => {
+            window.alert([p, t, a].join('\n'));
+        }, args: [platform, tabId, action],
+    });
+});
---
+++ src/context/meet-google-com.ts
@@ -0,0 +1,82 @@
+
+(async () => {
+    const MAX_RETRIES = 1200;
+    const RETRY_INTERVAL_MS = 100;
+    const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const indicator = document.createElement('div');
+
+    const timeinput = document.createElement('input');
+    timeinput.type = 'time';
+    const now = new Date();
+    const minutes = now.getMinutes();
+    const roundedMinutes = Math.ceil(minutes / 10) * 10;
+    now.setMinutes(roundedMinutes);
+    timeinput.value = now.toTimeString().substring(0, 5);
+
+    const joinbutton = document.createElement('button');
+    joinbutton.innerText = 'Join on time';
+    joinbutton.onclick = async () => {
+        const value = timeinput.value;
+        const [h, m] = value.split(':');
+        const scheduled = new Date();
+        scheduled.setHours(parseInt(h));
+        scheduled.setMinutes(parseInt(m));
+        scheduled.setSeconds(0);
+        scheduled.setMilliseconds(0);
+        await chrome.runtime.sendMessage(chrome.runtime.id, { action: 'schedule', scheduled: scheduled.getTime() });
+        timeinput.remove();
+        joinbutton.remove();
+        indicator.classList.add('scheduled');
+        indicator.innerText = `Scheduled to join at ${value}\nKeep this page open`;
+        container.style.border = "none";
+    };
+
+    const container = document.createElement('div');
+    container.id = 'join-on-time-container';
+
+    container.appendChild(indicator);
+    container.appendChild(timeinput);
+    container.appendChild(joinbutton);
+
+    let count = 0;
+    const retry = setInterval(async () => {
+        count++;
+        const ok = await render(container);
+        if (ok != null && ok.offsetWidth > 0) {
+            clearInterval(retry);
+        }
+        if (count > MAX_RETRIES) {
+            clearInterval(retry);
+        }
+    }, 1000);
+
+    const render = async (c: HTMLDivElement): Promise<HTMLDivElement | null> => {
+        const sibling = await waitUntilElementFound<HTMLButtonElement>('button', button => {
+            if (button.textContent == "Join now") return true;
+            if (button.textContent == "Switch here") return true;
+            return false;
+        });
+        const parent = sibling?.parentElement;
+        if (!sibling || !parent) return null;
+        // Insert this container next to sibling
+        // parent.insertBefore(c, sibling);
+        parent.appendChild(c);
+        // document.body.appendChild(container);
+        await sleep(1000);
+        return document.querySelector<HTMLDivElement>('#join-on-time-container');
+    }
+
+    const waitUntilElementFound = async <T extends HTMLElement>(selector: string, filter: (e: T) => boolean): Promise<T | null> => {
+        let count = 0;
+        return new Promise((resolve) => {
+            const interval = setInterval(() => {
+                count++;
+                const found = Array.from(document.querySelectorAll<T>(selector)).filter(filter);
+                if (found) { clearInterval(interval); resolve(found[0]); }
+                if (count > MAX_RETRIES) { clearInterval(interval); resolve(null); }
+            }, RETRY_INTERVAL_MS);
+        });
+    };
+
+})();
